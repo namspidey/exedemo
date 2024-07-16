37e515a56dbdf33b41462b802e0579f33804eed9
namspidey <namdepzaia4@gmail.com>
2024-07-16 08:57:17 +0700

    trans chat

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,138 +1,121 @@
-
+import React, { useState } from 'react';
 import '../style/Chat.css';
-import { FaChevronLeft } from "react-icons/fa";
-import { IoCall } from "react-icons/io5";
-import { FaVideo } from "react-icons/fa";
-import { FaPlusCircle } from "react-icons/fa";
-import { FaCamera } from "react-icons/fa6";
-import { FaImage } from "react-icons/fa6";
-import { FaMicrophone } from "react-icons/fa";
-import { IoSend } from "react-icons/io5";
-
+import { FaChevronLeft, FaVideo, FaPlusCircle, FaCamera, FaImage, FaMicrophone } from "react-icons/fa";
+import { IoCall, IoSend } from "react-icons/io5";
 import axios from 'axios';
-import * as signalR from '@microsoft/signalr';
-import React, { useState, useEffect, useRef } from 'react';
-import { HubConnectionBuilder } from '@microsoft/signalr';
+
+const translateToVietnamese = async (japaneseText) => {
+  const url = 'https://api.mymemory.translated.net/get';
+
+  try {
+    const response = await axios.get(url, {
+      params: {
+        q: japaneseText,
+        langpair: 'ja|vi'
+      }
+    });
+
+    if (response.data.responseStatus === 200) {
+      return response.data.responseData.translatedText;
+    } else {
+      throw new Error('Translation failed');
+    }
+  } catch (error) {
+    console.error('Translation error:', error);
+    return `[Translation Error: ${japaneseText}]`;
+  }
+};
 
 const Chat = () => {
-    
-        const [connection, setConnection] = useState(null);
-        const [messages, setMessages] = useState([]);
-        const [message, setMessage] = useState('');
-        const latestMessages = useRef(null);
-        const token = localStorage.getItem('token')
-        latestMessages.current = messages;
+    const [messages, setMessages] = useState([
+        { id: 1, sender: 'partner', vi: 'Xin chào, rất vui được làm quen với bạn', jp: 'こんにちは。はじめまして。よろしくね～' },
+        { id: 2, sender: 'self', vi: 'Chào bạn, mình cũng vậy', jp: 'こちらこそ' },
+        { id: 3, sender: 'partner', vi: 'Mình tên là A, còn bạn thì sao', jp: '私はAです。お名前は？' },
+        { id: 4, sender: 'self', vi: 'Tên mình là B', jp: 'Bです' },
+        { id: 5, sender: 'partner', vi: 'Bạn khoẻ không?', jp: 'お元 気ですか?' },
+        { id: 6, sender: 'self', vi: 'Tôi khỏe, còn bạn?', jp: '元気です。あなたは？' },
+    ]);
+    const [inputMessage, setInputMessage] = useState('');
 
-        useEffect(() => {
-            // Fetch past messages
-            const fetchMessages = async () => {
-                try {
-                    const response = await axios.get(
-                        'https://lombeo-api-authorize.azurewebsites.net/authen/messenger/Get-Messages?contactId=2',
-                        {
-                            headers: {
-                                'accept': '*/*',
-                                'Authorization': `Bearer ${token}`
-                            }
-                        }
-                    );
-                    setMessages(response.data);
-                } catch (error) {
-                    console.error('Error fetching messages:', error);
-                }
-            };
-
-            fetchMessages();
-
-            // Set up SignalR connection
-            const newConnection = new HubConnectionBuilder()
-                .withUrl('https://lombeo-api-authorize.azurewebsites.net/chatHub') // Update with your server URL
-                .withAutomaticReconnect()
-                .build();
-
-            setConnection(newConnection);
-        }, []);
-
-        useEffect(() => {
-            if (connection) {
-                connection.start()
-                    .then(() => {
-                        console.log('Connected!');
-
-                        connection.on('ReceiveMessage', (user, message) => {
-                            const updatedMessages = [...latestMessages.current];
-                            updatedMessages.push({ user, message });
-                            setMessages(updatedMessages);
-                        });
-                    })
-                    .catch(e => console.log('Connection failed: ', e));
-            }
-        }, [connection]);
-
-        const sendMessage = async () => {
-            if (connection && message) {
-                try {
-                    await connection.invoke('SendMessage', 'User', message);
-                    setMessage('');
-                } catch (e) {
-                    console.log('Send message failed: ', e);
-                }
-            }
-        };
-
-
-        return (
-            <div className='chat'>
-                <div className='chat-bar'>
-                    <div className='left-side'>
-                        <FaChevronLeft className='ic1' />
-                        <div className='bar-content'>
-                            Person A
-
-                        </div>
-                    </div>
-                    <div className='right-side'>
-                        <IoCall className='ic1' />
-                        <FaVideo className='ic1' />
-
-                    </div>
-                </div>
-                <div className='box'>
-                    <div className='box-chat'>
-                        {/* <div className='partner' data-lang='jp' data-vi='Xin chào, rất vui được làm quen với bạn' data-jp='こんにちは。はじめまして。よろしくね～' onClick={handleDivClick}>
-                    こんにちは。はじめまして。よろしくね～
-                    </div>
-                    <div className='self' data-lang='jp' data-vi='Chào bạn, mình cũng vậy' data-jp='こちらこそ' onClick={handleDivClick}>
-                    こちらこそ
-                    </div>
-                    <div className='partner' data-lang='jp' data-vi='Mình tên là A, còn bạn thì sao' data-jp='私はAです。お名前は？' onClick={handleDivClick}>
-                    私はAです。お名前は？
-                    </div>
-                    <div className='self' data-lang='jp' data-vi='Tên mình là B' data-jp='Bです' onClick={handleDivClick}>
-                    Bです
-                    </div>
-                    <div className='partner' data-lang='jp' data-vi='Bạn khoẻ không?' data-jp='お元 気ですか?' onClick={handleDivClick}>
-                    お元 気ですか?
-                    </div>
-                    <div className='self' data-lang='jp' data-vi='Tôi khỏe, còn bạn?' data-jp='元気です。あなたは？' onClick={handleDivClick}>
-                    元気です。あなたは？
-                    </div> */}
-
-                    </div>
-                    <div className='input-bar'>
-                        <FaPlusCircle className='ic1' />
-                        <FaCamera className='ic1' />
-                        <FaImage className='ic1' />
-
-                        <FaMicrophone className='ic1' />
-                        <input type='text' placeholder='Aa'></input>
-                        <IoSend className='ic1' />
-
-                    </div>
-                </div>
-
-            </div>
-        );
+    const toggleLanguage = (currentLang) => {
+        return currentLang === 'vi' ? 'jp' : 'vi';
     };
 
-    export default Chat;
+    const handleDivClick = (event) => {
+        const currentLang = event.target.dataset.lang;
+        const newLang = toggleLanguage(currentLang);
+        event.target.dataset.lang = newLang;
+        event.target.innerText = newLang === 'vi' ? event.target.dataset.vi : event.target.dataset.jp;
+    };
+
+    const handleInputChange = (event) => {
+        setInputMessage(event.target.value);
+    };
+
+    const handleSendMessage = async () => {
+        if (inputMessage.trim() !== '') {
+            try {
+                const vietnameseTranslation = await translateToVietnamese(inputMessage);
+                const newMessage = {
+                    id: messages.length + 1,
+                    sender: 'self',
+                    jp: inputMessage,
+                    vi: vietnameseTranslation
+                };
+                setMessages([...messages, newMessage]);
+                setInputMessage('');
+            } catch (error) {
+                console.error('Error sending message:', error);
+                // You might want to show an error message to the user here
+            }
+        }
+    };
+
+    return (
+        <div className='chat'>
+            <div className='chat-bar'>
+                <div className='left-side'>
+                    <FaChevronLeft className='ic1' />
+                    <div className='bar-content'>
+                        Person A
+                    </div>
+                </div>
+                <div className='right-side'>
+                    <IoCall className='ic1' />
+                    <FaVideo className='ic1' />
+                </div>
+            </div>
+            <div className='box'>
+                <div className='box-chat'>
+                    {messages.map((message) => (
+                        <div 
+                            key={message.id}
+                            className={message.sender}
+                            data-lang='jp'
+                            data-vi={message.vi}
+                            data-jp={message.jp}
+                            onClick={handleDivClick}
+                        >
+                            {message.jp}
+                        </div>
+                    ))}
+                </div>
+                <div className='input-bar'>
+                    <FaPlusCircle className='ic1' />
+                    <FaCamera className='ic1' />
+                    <FaImage className='ic1' />
+                    <FaMicrophone className='ic1' />
+                    <input 
+                        type='text' 
+                        placeholder='日本語で入力してください'
+                        value={inputMessage}
+                        onChange={handleInputChange}
+                    />
+                    <IoSend className='ic1' onClick={handleSendMessage} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Chat;
